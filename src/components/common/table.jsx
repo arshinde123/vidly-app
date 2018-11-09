112791c7a3353fc1ab496160668359e2cae2d785
Arvind <arvind@Knorexs-MacBook-Pro-2.local>
2018-11-09 08:58:31 +0530

    pwa support added

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -4,7 +4,7 @@ import TableBody from "./tableBody";
 
 const Table = ({ columns, sortColumn, onSort, data }) => {
   return (
-    <table className="table table-hover">
+    <table className="table table-hover" style={{ overflow : 'scroll' }}>
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
       <TableBody data={data} columns={columns} />
     </table>
